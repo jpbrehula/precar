e752e567697b32ec88bf2131ca85fba32df7b9f5
João Pedro Brehula <jpbrehula@hotmail.com>
2025-12-28 18:12:03 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Ola, essa e a primeira pagina da precar,um projeto que irá revolucionar o mercado de compra e venda de carros. </h1>
+    return <h1>Ola, essa e a primeira pagina da precar,um projeto que irá revolucionar o mercado de compra e venda de carros. Esse é o começo </h1>
 }
 
 export default Home;
