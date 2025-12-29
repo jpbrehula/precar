3a45c4f965b5c3f83e5f18af27efa00513d55934
João Pedro Brehula <jpbrehula@hotmail.com>
2025-12-29 20:43:59 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # precar
+
 Precar é uma plataforma de pré-compra automotiva guiada por IA, projetada para ajudar os compradores de carros a fazerem uma inspeção completa e técnica antes de realizar a compra, gerando um laudo detalhado e seguro.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Ola, essa e a primeira pagina da precar,um projeto que irá revolucionar o mercado de compra e venda de carros. Esse é o começo </h1>
+  return (
+    <h1>
+      Ola, essa e a primeira pagina da precar,um projeto que irá revolucionar o
+      mercado de compra e venda de carros. Esse é o começo{" "}
+    </h1>
+  );
 }
 
 export default Home;
